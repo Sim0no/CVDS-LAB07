82c548d7da4f3f3704bc52b19af194dbfa227638
Simón Marín <46967217+SimonMarinM@users.noreply.github.com>
2019-10-03 15:06:51 -0500

    Revert "Revert "Refactoring""
    This reverts commit c4f56537a47ace14a51176887a533260b5cfb217.

---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ItemRentadoMapper.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.sampleprj.dao.mybatis.mappers;
+
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import edu.eci.cvds.samples.entities.TipoItem;
+
+public interface ItemRentadoMapper {
+    
+    
+
+}
